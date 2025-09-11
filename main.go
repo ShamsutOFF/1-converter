ba26564f64083bab5c03bfa1f56ea8a24f7b2115
Adel <shamsutoff@yandex.ru>
2025-09-11 17:26:25 +0300

    dz2

--- main.go
+++ main.go
@@ -13,3 +13,15 @@ func main() {
 	fmt.Printf("1 USD = %.2f RUB\n", UsdToRub)
 	fmt.Printf("1 EUR = %.2f RUB\n", EurToRub)
 }
+func convert(sum float64, fromV, toV string) float64 {
+	return 0
+}
+
+func readUsersInput() float64 {
+	var input float64
+	_, err := fmt.Scan(&input)
+	if err != nil {
+		return 0
+	}
+	return input
+}
