f5d39df25a6c132ce89cfa8847cd3f5b48f5944a
Adel <shamsutoff@yandex.ru>
2025-09-11 08:53:12 +0300

    dz 1 done

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+const (
+	UsdToEur = 0.85
+	UsdToRub = 85.00
+	EurToRub = UsdToRub / UsdToEur
+)
+
+func main() {
+	fmt.Printf("1 USD = %.2f EUR\n", UsdToEur)
+	fmt.Printf("1 USD = %.2f RUB\n", UsdToRub)
+	fmt.Printf("1 EUR = %.2f RUB\n", EurToRub)
 }
