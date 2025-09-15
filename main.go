8f0a0bb9e5d39db245408642be057b27e1e00781
Adel <shamsutoff@yandex.ru>
2025-09-15 07:27:42 +0300

    DZ-4: Arrays and Slices

--- main.go
+++ main.go
@@ -1,5 +1,103 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	fmt.Println("___ Добро пожаловать в калькулятор ___")
+	var operation string
+	for {
+		op, err := readOperation()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		operation = op
+		break
+	}
+
+	fmt.Println("Операция:", operation)
+	var nums []int
+	for {
+		ints, err := readNumbers()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		nums = ints
+		break
+	}
+	fmt.Println("Вы ввели: ", nums)
+	var result float64
+	switch operation {
+	case "AVG":
+		for _, num := range nums {
+			result += float64(num)
+		}
+		result /= float64(len(nums))
+	case "SUM":
+		for _, num := range nums {
+			result += float64(num)
+		}
+	case "MED":
+		sort.Ints(nums)
+		if len(nums)%2 == 0 {
+			i := len(nums) / 2
+			result = (float64(nums[i]) + float64(nums[i-1])) / 2.0
+		} else {
+			result = float64(nums[len(nums)/2])
+		}
+	}
+	fmt.Println("Результат операции: ", result)
+}
+
+func readOperation() (string, error) {
+	fmt.Print("Введите целевую операцию (AVG - среднее, SUM - сумму или MED - медиану): ")
+	input, err := readInput()
+	if err != nil {
+		return "", err
+	}
+
+	input = strings.ToUpper(strings.TrimSpace(input))
+	if input != "AVG" && input != "SUM" && input != "MED" {
+		return "", errors.New("вы ввели не верную операцию! Попробуйте снова")
+	}
+
+	return input, nil
+}
+
+func readNumbers() ([]int, error) {
+	fmt.Print("Введите числа через запятую: ")
+	input, err := readInput()
+	if err != nil {
+		return nil, err
+	}
+	input = strings.TrimSpace(input)
+	slStr := strings.Split(input, ",")
+	var slInt = make([]int, len(slStr))
+	for i, v := range slStr {
+		v = strings.TrimSpace(v)
+		atoi, err := strconv.Atoi(v)
+		if err != nil {
+			return nil, err
+		}
+		slInt[i] = atoi
+	}
+	return slInt, nil
+}
+
+func readInput() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return "", errors.New("ошибка чтения ввода")
+	}
+	return strings.TrimSpace(input), nil
 }
